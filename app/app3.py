caf01195e058768f9e3fe515fc357f088750905a
iojabal <iojabal9@gmail.com>
2023-06-18 18:39:39 -0400

    Make a first Iteration

--- app/app3.py
+++ app/app3.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 from models.Matrix import Matrix
+from models.Etapa import Etapa
 app = Flask(__name__)
 
 
-matrix = Matrix()
+ProblemMatrix = Matrix()
+cost = None
 
 initial_data = {
     "min_asig": None,
@@ -21,26 +23,34 @@ def hello_function():
 def get_Initial_Data():
     requested_data = request.json
     size = requested_data.get('size')
-    initial_data["Matrix"] = matrix.set_Size_of_Matrix(size[0], size[1]).tolist()
+    initial_data["Matrix"] = ProblemMatrix.set_Size_of_Matrix(size[0], size[1]).tolist()
     initial_data["min_asig"] = {f'M{x}': 1 for x in range(1, size[0]+1)}
     initial_data["available_res"] = requested_data.get("available_res")
     initial_data["optimizer"] = requested_data.get("optimizer")
+    print(ProblemMatrix.matrix)
     return initial_data
 
 
 @app.route("/fill_matrix", methods=['POST'])
 def show_loaded_data():
+    global cost
     requested_data = request.json
     matrixLocaly = requested_data.get("matrix")
-    matrix.fill_Matrix(matrixLocaly)
-    r = matrix.getBenefits(0)
-    return {"matrix": matrix.matrix.tolist(), "r": r}
+    ProblemMatrix.fill_Matrix(matrixLocaly)
+    cost = ProblemMatrix.getBenefits(2)
+    print(ProblemMatrix.matrix)
+    return {"matrix": ProblemMatrix.matrix.tolist(), "r": cost}
 
 @app.route("/solution", methods=["POST"])
 def make_Solution():
+    global cost
     requested_data = request.json
-    return requested_data
-
+    f = requested_data.get('f')
+    etapa = Etapa()
+    etapa.set_Size_of_Matrix(requested_data.get("range"), len(requested_data.get("nro")))
+    etapa.iterations(cost, f)
+    print(etapa.matrix)
+    return {"r": cost}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/models/Etapa.py
+++ app/models/Etapa.py
@@ -1,7 +1,35 @@
-from Matrix import Matrix
+import numpy as np
+
+
 class Etapa:
-    def __init__(self, rango, nro) -> None:
-        self.matrix = Matrix()
-        self.matrix.set_Size_of_Matrix(nro, rango)
+    def __init__(self) -> None:
+        self.rows = 0
+        self.columns = 0
+        self.matrix = np.matrix
+    def set_Size_of_Matrix(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = np.zeros([rows, columns])
+        return self.matrix
+
+    # return self.matrix[:, objetive].tolist()
+
+    def process(self, r, f):
+        for fila in range(self.rows):
+            for col in range(self.columns):
+                if col <= fila:
+                    self.matrix[fila, col] = r[col][0] + f[col]
+
+    def maxOrMin(self, case='Max'):
+        if case.lower() == 'max':
+            return np.amax(self.matrix, axis=1)
+        else:
+            return np.ma.masked_equal(self.matrix, 0).min(axis=1)
+
     def iterations(self, r, f):
-        pass
+        self.process(r, f)
+        print(self.matrix)
+        f = self.maxOrMin("min")
+
+
+
